bf126e500132bd625ea6fdf83b872d0df550b96b
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-05-07 11:39:41 +0530

    WIP: service-page/add feature section

--- frontend/src/patient/Services.jsx
+++ frontend/src/patient/Services.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Row, Col, Card, Form } from "react-bootstrap";
 import "./Services.css";
 import { FcPlus } from "react-icons/fc";
 import {
@@ -7,8 +8,33 @@ import {
 } from "react-icons/md";
 import { IoTimeOutline, IoLocationOutline } from "react-icons/io5";
 import { IoPersonOutline } from "react-icons/io5";
+import {
+  FaAmbulance,
+  FaHospitalSymbol,
+  FaUserNurse,
+  FaStethoscope,
+} from "react-icons/fa";
 
 const Services = () => {
+  const serviceFeatureCards = [
+    {
+      title: "24/7 Emergency Ambulance Service",
+      icon: <FaAmbulance className="feature-icon" />,
+    },
+    {
+      title: "Multi-Specialty Healthcare Services",
+      icon: <FaHospitalSymbol className="feature-icon" />,
+    },
+    {
+      title: "Professional Nursing Assistance",
+      icon: <FaUserNurse className="feature-icon" />,
+    },
+    {
+      title: "Comprehensive Health Checkups",
+      icon: <FaStethoscope className="feature-icon" />,
+    },
+  ];
+
   const allServices = [
     {
       id: 1,
@@ -68,49 +94,79 @@ const Services = () => {
     <div className="service-container">
       <div className="title-section">
         <h1>Diagnostic & Lab Services</h1>
-        <button className="add-btn" onClick={handleNewService}>
-          <FcPlus className="icon" />
-          Add New Service
-        </button>
+        <p>
+          We offer fast, accurate diagnostic and lab services including blood
+          tests, scans, and health checkups — all under one roof, ensuring
+          timely results and trusted care for your peace of mind.
+        </p>
+
+        <Row className="feature-section">
+          {serviceFeatureCards.map((card, index) => (
+            <Col md={3} sm={6} className="mb-4" key={index}>
+              <Card className="service-feature-card">
+                <Card.Body className="text-center">
+                  <Card.Title className="feature-title">
+                    <span className="feature-icon me-2">{card.icon}</span>
+                    <span>{card.title}</span>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <div className="buttons">
+          <button className="add-btn" onClick={handleNewService}>
+            Add New Service
+          </button>
+          <button className="view-service-btn">View My Service</button>
+        </div>
       </div>
+      {allServices.length > 0 ? (
+        <>
+          <p className="subtitle">
+            Here are your upcoming services. We’re with you every step of the
+            way!
+          </p>
 
-      <p className="subtitle">
-        Here are your upcoming services. We’re with you every step of the way!
-      </p>
-
-      <div className="service-card-wrapper">
-        {allServices.map((service) => (
-          <div className="service-card" key={service.id}>
-            {/* Card header */}
-            <div className="card-header">
-              <h3>{service.name}</h3>
-            </div>
-            {/* Card body */}
-            <div className="card-body">
-              <p>
-                <MdOutlineFormatListNumbered className="icon" />
-                <strong>Appointment No:</strong>&nbsp;{service.id}
-              </p>
-              <p>
-                <IoPersonOutline className="icon" />
-                <strong>Patient Name:</strong>&nbsp;{service.id}
-              </p>
-              <p>
-                <MdOutlineDateRange className="icon" />
-                <strong>Date:</strong>&nbsp;{service.date}
-              </p>
-              <p>
-                <IoTimeOutline className="icon" />
-                <strong>Time:</strong>&nbsp;{service.time}
-              </p>
-              <p>
-                <IoLocationOutline className="icon" />
-                <strong>Location:</strong>&nbsp;{service.roomNum}
-              </p>
-            </div>
+          <div className="service-card-wrapper">
+            {allServices.map((service) => (
+              <div className="service-card" key={service.id}>
+                {/* Card header */}
+                <div className="card-header">
+                  <h3>{service.name}</h3>
+                </div>
+                {/* Card body */}
+                <div className="card-body">
+                  <p>
+                    <MdOutlineFormatListNumbered className="icon" />
+                    <strong>Appointment No:</strong>&nbsp;{service.id}
+                  </p>
+                  <p>
+                    <IoPersonOutline className="icon" />
+                    <strong>Patient Name:</strong>&nbsp;{service.id}
+                  </p>
+                  <p>
+                    <MdOutlineDateRange className="icon" />
+                    <strong>Date:</strong>&nbsp;{service.date}
+                  </p>
+                  <p>
+                    <IoTimeOutline className="icon" />
+                    <strong>Time:</strong>&nbsp;{service.time}
+                  </p>
+                  <p>
+                    <IoLocationOutline className="icon" />
+                    <strong>Location:</strong>&nbsp;{service.roomNum}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      ) : (
+        <div className="empty-service">
+          <p>No pending appointments at the moment. Please book one!</p>
+        </div>
+      )}
 
       {/*New service Modal */}
       {showModel && (
