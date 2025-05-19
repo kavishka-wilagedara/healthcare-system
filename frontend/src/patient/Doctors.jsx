9ecc7aaf9b56fd6246598506e90e383c3216d786
chamuda-arangalla <chamudaarangalla@gmail.com>
2025-05-19 12:04:20 +0530

    clear the code

--- frontend/src/patient/Doctors.jsx
+++ frontend/src/patient/Doctors.jsx
@@ -29,53 +29,6 @@ const Doctors = () => {
       fetchDocAvailableTimes();
     }, []);
   
-  // const doctors = [
-  //   {
-  //     id: "1",
-  //     name: "Dr. Kavindra Perera",
-  //     speciality: "Cardiologist",
-  //     availableDate: "2025-05-18",
-  //     availableTime: "09:00 - 12:00",
-  //     imgUrl:
-  //       "https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: "2",
-  //     name: "Dr. Nadeesha Fernando",
-  //     speciality: "Dermatologist",
-  //     availableDate: "2025-05-19",
-  //     availableTime: "13:00 - 16:00",
-  //     imgUrl:
-  //       "https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: "3",
-  //     name: "Dr. Ruwan Jayasuriya",
-  //     speciality: "Orthopedic Surgeon",
-  //     availableDate: "2025-05-20",
-  //     availableTime: "10:00 - 14:00",
-  //     imgUrl:
-  //       "https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: "4",
-  //     name: "Dr. Maleesha Wijeratne",
-  //     speciality: "Pediatrician",
-  //     availableDate: "2025-05-21",
-  //     availableTime: "08:00 - 11:30",
-  //     imgUrl:
-  //       "https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: "5",
-  //     name: "Dr. Hashan Karunaratne",
-  //     speciality: "Neurologist",
-  //     availableDate: "2025-05-22",
-  //     availableTime: "14:00 - 17:00",
-  //     imgUrl: "",
-  //   },
-  // ];
-
   const handleBooking = (doctor) => {
     Swal.fire({
       icon: "question",
